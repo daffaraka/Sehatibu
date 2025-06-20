55fdb735eb63e070eb1a6f6e270255f30cea2c5e
Daffa Raka <daffarakals@gmail.com>
2025-06-20 15:59:46 +0700

    Setup base layout

---
+++ resources/views/dashboard/partials/sidebar.blade.php
@@ -0,0 +1,78 @@
+  <div class="sidebar" data-background-color="dark">
+      <div class="sidebar-logo">
+          <!-- Logo Header -->
+          <div class="logo-header" data-background-color="dark">
+              <a href="index.html" class="logo">
+                  <img src="{{ asset('kai-admin/assets/img/kaiadmin/logo_light.svg') }}" alt="navbar brand"
+                      class="navbar-brand" height="20" />
+              </a>
+              <div class="nav-toggle">
+                  <button class="btn btn-toggle toggle-sidebar">
+                      <i class="gg-menu-right"></i>
+                  </button>
+                  <button class="btn btn-toggle sidenav-toggler">
+                      <i class="gg-menu-left"></i>
+                  </button>
+              </div>
+              <button class="topbar-toggler more">
+                  <i class="gg-more-vertical-alt"></i>
+              </button>
+          </div>
+          <!-- End Logo Header -->
+      </div>
+      <div class="sidebar-wrapper scrollbar scrollbar-inner">
+          <div class="sidebar-content">
+              <ul class="nav nav-secondary">
+                  <li class="nav-item">
+                      <a href="dashboard.html">
+                          <i class="fas fa-tachometer-alt"></i>
+                          <p>Dashboard</p>
+                          <span class="badge badge-success">4</span>
+                      </a>
+                  </li>
+                  <li class="nav-item">
+                      <a href="user.html">
+                          <i class="fas fa-user-friends"></i>
+                          <p>User</p>
+                          <span class="badge badge-warning">3</span>
+                      </a>
+                  </li>
+                  <li class="nav-item">
+                      <a href="hasil-input-user.html">
+                          <i class="fas fa-list"></i>
+                          <p>Hasil Input User</p>
+                          <span class="badge badge-info">2</span>
+                      </a>
+                  </li>
+                  <li class="nav-item">
+                      <a href="hasil-ahp.html">
+                          <i class="fas fa-chart-pie"></i>
+                          <p>Hasil AHP</p>
+                          <span class="badge badge-primary">1</span>
+                      </a>
+                  </li>
+                  <li class="nav-item">
+                      <a href="makanan.html">
+                          <i class="fas fa-store"></i>
+                          <p>Menu Tersedia</p>
+                          <span class="badge badge-info">1</span>
+                      </a>
+                  </li>
+                  <li class="nav-item">
+                      <a href="makanan.html">
+                          <i class="fas fa-utensils"></i>
+                          <p>Makanan</p>
+                          <span class="badge badge-secondary">1</span>
+                      </a>
+                  </li>
+                  {{-- <li class="nav-item">
+                      <a href="widgets.html">
+                          <i class="fas fa-image"></i>
+                          <p>Gallery</p>
+                          <span class="badge badge-danger">4</span>
+                      </a>
+                  </li> --}}
+              </ul>
+          </div>
+      </div>
+  </div>
